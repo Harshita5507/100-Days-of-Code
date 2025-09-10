5b4c5bd87fd64328be5e0a86a1c1d9d69572925f
Harshita5507 <hershey0057@gmail.com>
2025-09-10 23:57:44 +0530

    Update Day5-q2.c

--- Day5-q2.c
+++ Day5-q2.c
@@ -1,21 +1,19 @@
+//Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+# include <math.h>
 # include <stdio.h>
 int main () {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    int total_Seconds, hours,seconds,minutes;
 
-    if(num>=0) {
-        if (num==0) 
-        {
-            printf("The number is Zero.");
-        }
-        else {
-            printf("The number is positive.");
-        }
+    printf("enter time in seconds: ");
+    scanf("%d", &total_Seconds);
 
-    }
-    else {
-        printf("The number is Negative.");
-    }
+    hours= seconds/3600;
+    total_Seconds= total_Seconds % 3600;
+
+    minutes= total_Seconds/60;
+    seconds= total_Seconds % 60;
+
+    printf("hours:minutes:seconds- %02d %02d %02d", hours, minutes, seconds);
+    
     return 0;
 }
