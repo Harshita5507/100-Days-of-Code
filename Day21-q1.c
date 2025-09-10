a15db5dcd83add2fd273228ab8689e9be423d814
Harshita5507 <hershey0057@gmail.com>
2025-09-11 00:36:50 +0530

    Add files via upload

---
+++ Day21-q1.c
@@ -0,0 +1,17 @@
+//Write a program to swap the first and last digit of a number.
+#include <stdio.h>
+
+int main() {
+    int num,i;
+    printf("Enter a number that is to be reversed: ");
+    scanf("%d", num);
+
+    while (num>0){
+        
+        num;
+    }
+
+
+    
+    return 0;
+}
