3c781e4c3ddbf2ce21e185275927816c274b8802
Harshita5507 <hershey0057@gmail.com>
2025-09-10 17:57:55 +0530

    Day19-q2.c

--- Day19-q2.c
+++ Day19-q2.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 int main()
 {
-int num,sum;
+int num,sum=0,digit;
 
 printf("Enter a number whose sum of digits is to be found: ");
 scanf("%d", num);
 
-sum=
-
-
-
- return 0;
+while (num !=0)
+{
+    digit= num%10;
+    sum += digit;
+    num/=10;
+}
+     printf("the sum of digits is: %d", sum);
+    return 0;
 }
