51329d72dec4da5ab844e73f7d235153c96d620e
Harshita5507 <hershey0057@gmail.com>
2025-09-26 19:52:43 +0530

    Add files via upload

--- Day33-q2.c
+++ Day33-q2.c
@@ -2,10 +2,39 @@
 #include <stdio.h>
 int main ()
 {
-    int;
+    int n,new_element,i,index;
 
+    printf("enter n: ");
+    scanf("%d", &n);
 
+    printf("enter elements of array: ");
 
+    int arr[n+1];
+
+    for (i=0;i<n;i++)
+    scanf("%d", &arr[i]);
+
+    printf("enter the element to be inserted at the position: ");
+    scanf("%d",&new_element);
+
+    for (i=0;i<n;i++)
+    {
+       if (arr[i]>new_element)
+       {
+         break;
+       }
+    }
+
+    for (int j=n; j>i ;j--)
+    {
+        arr[j]=arr[j-1];
+    }
+
+    arr[i]=new_element;
+    n++;
+
+    for (i=0;i<n;i++)
+    printf("%d ", arr[i]);
 
     return 0;
 }
