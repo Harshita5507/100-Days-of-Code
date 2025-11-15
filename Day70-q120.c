2ce6821a3221aef983f1be6e3b9eaaaf5a026142
Harshita5507 <hershey0057@gmail.com>
2025-11-15 23:28:37 +0530

    Add files via upload

---
+++ Day70-q120.c
@@ -0,0 +1,19 @@
+//Write a program to take a string input. Change it to sentence case.
+#include <stdio.h>
+int main()
+{
+    int i;
+    char str[50];
+    printf("enter a a string");
+    scanf("%[^\n]", &str);
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
